2c39566c5ab388b2fc46dfeb8d61d64a87b8d043
Benjamin Rojas-Ritchie <ben@benarr.com>
2021-09-22 18:35:42 -0600

    feature: add aos/remove js/preliminary styling

--- .eleventy.js
+++ .eleventy.js
@@ -4,116 +4,125 @@ const UglifyJS = require("uglify-js");
 const htmlmin = require("html-minifier");
 const eleventyNavigationPlugin = require("@11ty/eleventy-navigation");
 
-module.exports = function(eleventyConfig) {
+module.exports = function (eleventyConfig) {
+    // Eleventy Navigation https://www.11ty.dev/docs/plugins/navigation/
+    eleventyConfig.addPlugin(eleventyNavigationPlugin);
 
-  // Eleventy Navigation https://www.11ty.dev/docs/plugins/navigation/
-  eleventyConfig.addPlugin(eleventyNavigationPlugin);
+    // Configuration API: use eleventyConfig.addLayoutAlias(from, to) to add
+    // layout aliases! Say you have a bunch of existing content using
+    // layout: post. If you don’t want to rewrite all of those values, just map
+    // post to a new file like this:
+    // eleventyConfig.addLayoutAlias("post", "layouts/my_new_post_layout.njk");
 
-  // Configuration API: use eleventyConfig.addLayoutAlias(from, to) to add
-  // layout aliases! Say you have a bunch of existing content using
-  // layout: post. If you don’t want to rewrite all of those values, just map
-  // post to a new file like this:
-  // eleventyConfig.addLayoutAlias("post", "layouts/my_new_post_layout.njk");
+    // Merge data instead of overriding
+    // https://www.11ty.dev/docs/data-deep-merge/
+    eleventyConfig.setDataDeepMerge(true);
 
-  // Merge data instead of overriding
-  // https://www.11ty.dev/docs/data-deep-merge/
-  eleventyConfig.setDataDeepMerge(true);
+    // Add support for maintenance-free post authors
+    // Adds an authors collection using the author key in our post frontmatter
+    // Thanks to @pdehaan: https://github.com/pdehaan
+    eleventyConfig.addCollection("authors", (collection) => {
+        const blogs = collection.getFilteredByGlob("posts/*.md");
+        return blogs.reduce((coll, post) => {
+            const author = post.data.author;
+            if (!author) {
+                return coll;
+            }
+            if (!coll.hasOwnProperty(author)) {
+                coll[author] = [];
+            }
+            coll[author].push(post.data);
+            return coll;
+        }, {});
+    });
 
-  // Add support for maintenance-free post authors
-  // Adds an authors collection using the author key in our post frontmatter
-  // Thanks to @pdehaan: https://github.com/pdehaan
-  eleventyConfig.addCollection("authors", collection => {
-    const blogs = collection.getFilteredByGlob("posts/*.md");
-    return blogs.reduce((coll, post) => {
-      const author = post.data.author;
-      if (!author) {
-        return coll;
-      }
-      if (!coll.hasOwnProperty(author)) {
-        coll[author] = [];
-      }
-      coll[author].push(post.data);
-      return coll;
-    }, {});
-  });
+    // Date formatting (human readable)
+    eleventyConfig.addFilter("readableDate", (dateObj) => {
+        return DateTime.fromJSDate(dateObj).toFormat("dd LLL yyyy");
+    });
 
-  // Date formatting (human readable)
-  eleventyConfig.addFilter("readableDate", dateObj => {
-    return DateTime.fromJSDate(dateObj).toFormat("dd LLL yyyy");
-  });
+    // Date formatting (machine readable)
+    eleventyConfig.addFilter("machineDate", (dateObj) => {
+        return DateTime.fromJSDate(dateObj).toFormat("yyyy-MM-dd");
+    });
 
-  // Date formatting (machine readable)
-  eleventyConfig.addFilter("machineDate", dateObj => {
-    return DateTime.fromJSDate(dateObj).toFormat("yyyy-MM-dd");
-  });
+    /*   // Minify JS
+      eleventyConfig.addFilter("jsmin", function (code) {
+        let minified = UglifyJS.minify(code);
+        if (minified.error) {
+          console.log("UglifyJS error: ", minified.error);
+          return code;
+        }
+        return minified.code;
+      }); */
 
-  // Minify JS
-  eleventyConfig.addFilter("jsmin", function(code) {
-    let minified = UglifyJS.minify(code);
-    if (minified.error) {
-      console.log("UglifyJS error: ", minified.error);
-      return code;
-    }
-    return minified.code;
-  });
+    /*   // Minify HTML output
+      eleventyConfig.addTransform("htmlmin", function (content, outputPath) {
+        if (outputPath.indexOf(".html") > -1) {
+          let minified = htmlmin.minify(content, {
+            useShortDoctype: true,
+            removeComments: true,
+            collapseWhitespace: true,
+          });
+          return minified;
+        }
+        return content;
+      }); */
 
-  // Minify HTML output
-  eleventyConfig.addTransform("htmlmin", function(content, outputPath) {
-    if (outputPath.indexOf(".html") > -1) {
-      let minified = htmlmin.minify(content, {
-        useShortDoctype: true,
-        removeComments: true,
-        collapseWhitespace: true
-      });
-      return minified;
-    }
-    return content;
-  });
+    // Don't process folders with static assets e.g. images
+    eleventyConfig.addPassthroughCopy("favicon.ico");
+    eleventyConfig.addPassthroughCopy("static/img");
+    eleventyConfig.addPassthroughCopy("admin");
+    eleventyConfig.addPassthroughCopy({
+        "./_includes/assets/css/style.css": "./css/style.css",
+    });
+    eleventyConfig.addPassthroughCopy({
+        "./node_modules/alpinejs/dist/cdn.js": "./js/alpine.js",
+    });
+    eleventyConfig.addPassthroughCopy({
+        "./node_modules/aos/dist/aos.js": "./js/aos.js",
+    });
+    eleventyConfig.addPassthroughCopy({
+        "./node_modules/aos/dist/aos.css": "./css/aos.css",
+    });
+    eleventyConfig.addPassthroughCopy({
+        "./_includes/assets/js/inline.js": "./js/inline.js",
+    });
 
-  // Don't process folders with static assets e.g. images
-  eleventyConfig.addPassthroughCopy("favicon.ico");
-  eleventyConfig.addPassthroughCopy("static/img");
-  eleventyConfig.addPassthroughCopy("admin");
-  eleventyConfig.addPassthroughCopy({
-    './_includes/assets/css/style.css': './style.css'
-  });
-  eleventyConfig.addPassthroughCopy({
-    './node_modules/alpinejs/dist/cdn.js': './js/alpine.js',
-  })
+    /* Markdown Plugins */
+    let markdownIt = require("markdown-it");
+    let markdownItAnchor = require("markdown-it-anchor");
+    let options = {
+        html: true,
+        breaks: true,
+        linkify: true,
+    };
+    let opts = {
+        permalink: false,
+    };
 
-  /* Markdown Plugins */
-  let markdownIt = require("markdown-it");
-  let markdownItAnchor = require("markdown-it-anchor");
-  let options = {
-    html: true,
-    breaks: true,
-    linkify: true
-  };
-  let opts = {
-    permalink: false
-  };
+    eleventyConfig.setLibrary(
+        "md",
+        markdownIt(options).use(markdownItAnchor, opts)
+    );
 
-  eleventyConfig.setLibrary("md", markdownIt(options)
-    .use(markdownItAnchor, opts)
-  );
+    return {
+        templateFormats: ["md", "njk", "html", "liquid"],
 
-  return {
-    templateFormats: ["md", "njk", "html", "liquid"],
+        // If your site lives in a different subdirectory, change this.
+        // Leading or trailing slashes are all normalized away, so don’t worry about it.
+        // If you don’t have a subdirectory, use "" or "/" (they do the same thing)
+        // This is only used for URLs (it does not affect your file structure)
+        pathPrefix: "/",
 
-    // If your site lives in a different subdirectory, change this.
-    // Leading or trailing slashes are all normalized away, so don’t worry about it.
-    // If you don’t have a subdirectory, use "" or "/" (they do the same thing)
-    // This is only used for URLs (it does not affect your file structure)
-    pathPrefix: "/",
-
-    markdownTemplateEngine: "liquid",
-    htmlTemplateEngine: "njk",
-    dataTemplateEngine: "njk",
-    dir: {
-      input: ".",
-      includes: "_includes",
-      data: "_data",
-      output: "_site"
-    }
-  };
+        markdownTemplateEngine: "liquid",
+        htmlTemplateEngine: "njk",
+        dataTemplateEngine: "njk",
+        dir: {
+            input: ".",
+            includes: "_includes",
+            data: "_data",
+            output: "_site",
+        },
+    };
 };
--- tailwind.config.js
+++ tailwind.config.js
@@ -2,10 +2,19 @@ module.exports = {
   purge: [ '_site/**/*.html','_site/*.html' ],
   darkMode: false, // or 'media' or 'class'
   theme: {
-    extend: {},
+    extend: {
+        fontFamily: {
+            "martel-sans": "'Martel Sans',sans-serif",
+        },
+        colors: {
+            "site-yellow": "#FEBD2F",
+            "site-tan": "#E7DACD",
+        }
+    },
   },
   variants: {
-    extend: {},
+    extend: {
+    },
   },
   plugins: [],
 }
